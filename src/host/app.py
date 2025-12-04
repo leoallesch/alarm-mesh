5aef3e732f1734815d255cf070e6dc6512c0464a
N1cYam <nicolas@yamnitz.com>
2025-12-04 15:10:36 -0500

    modified:   requirements.txt 	modified:   src/host/app.py 	modified:   src/host/templates/index.html
        The Website is up and working now

--- src/host/app.py
+++ src/host/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, send_from_directory
-from alarm import AlarmHost
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+from wtforms_components import TimeField
+from datetime import datetime
 
 app = Flask(__name__)
-host = AlarmHost()
+app.config['SECRET_KEY'] = "secretkey"
 
-@app.route("/")
-def home():
-    return "Test"
+class AlarmTime(FlaskForm):
+    time = TimeField('Time', validators = [InputRequired()])
+    submit = SubmitField("Set Alarm")
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    form = AlarmTime()
+    if form.validate_on_submit():
+        t = form.time.data
+        #print(t)
+        return f"Time the alarm will go off will be: {t}"
+    return render_template("index.html", form = form)
 
 
 if __name__ == "__main__":
