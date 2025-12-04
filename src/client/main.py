1505a5fd6a60c0881c3fd00008e35c991749bb60
Leo Allesch <leoallesch@gmail.com>
2025-12-04 16:23:57 -0500

    update

--- src/client/main.py
+++ src/client/main.py
@@ -101,6 +101,7 @@ def main():
     # Initialize LED
     try:
         led = LedController(pin=24)
+        led.on()
         print("[NODE APP] LED initialized")
     except Exception as e:
         print(f"[NODE APP] Failed to initialize LED: {e}")
